d17925fc3c5927c79df524ada9670c199040b810
Maksym Pinchuk <maksympc@gmail.com>
2018-04-09 20:07:47 +0300

    conver login and chat page to jade template, pass username to chat page, add titles for pages

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,18 @@
 var express = require('express');
-var path    = require("path");
 var router = express.Router();
 
 /* GET home     page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(path.dirname(module.parent.filename),'/views/main.html'));
+  res.render('index',{title:'ChatApp. Login page'});
 });
 
 router.get('/chat', function(req, res, next) {
-    res.sendFile(path.join(path.dirname(module.parent.filename),'/views/chat.html'));
+    res.render('chat',{title:'ChatApp. Chat page', username:'empty'});
 });
 
-//redirect to router
 router.post('/chat',function(req,res,next){
     console.log("body:" + JSON.stringify(req.body));
-    res.username = req.body.username;
-    res.sendFile(path.join(path.dirname(module.parent.filename),'/views/chat.html'));
+    res.render('chat',{title:'ChatApp. Chat page', username:'empty', username:req.body.username.toString()});
 });
 
 module.exports = router;
